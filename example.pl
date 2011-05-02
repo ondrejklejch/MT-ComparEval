8d637bbdac58bc3ac61698ccb1fbcb5c963c2aa9
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-05-02 18:17:34 +0200

    Implementation of bleu computation in Perl added.
    First attempt to port bleu computation into perl script. The algorithm
    is placed in NGram.pm module.

---
+++ example.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use NGram;
+
+$ngram = new NGram();
+
+open my $reference_file, '<', 'reference.txt';
+open my $machine_file, '<', 'machine.txt';
+
+my @reference_translations;
+my @machine_translations;
+
+while( my $reference = <$reference_file> ) {
+	chomp( $reference );
+	push( @reference_translations, $reference ); 
+} 
+
+while( my $machine = <$machine_file> ) {
+	chomp( $machine );
+	push( @machine_translations, $machine );
+}
+
+while( $#reference_translations >= 0 ) {
+	$ngram->add_sentence(
+		pop( @reference_translations ),
+		pop( @machine_translations )
+	);
+}
+
+print $ngram->get_bleu();
+
+close( $reference_file );
+close( $machine_file );
+
+
