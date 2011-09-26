6f29012ffdd829c30ea2a8f0017aa0797cef9d40
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-09-26 08:35:08 +0200

    Scripts for diff displaying added.

--- prototype/js/control.js
+++ prototype/js/control.js
@@ -1,51 +1,58 @@
-$("#sort-control").click(function() {
-	var callback;
-	var newOrder;
+$(document).ready(function() {
+	$("#sort-control").click(function() {
+		var callback;
+		var newOrder;
 		
-	if($(this).children("span").text() == "DESC") {
-		callback = function sortDescending(a, b) {
-			var valueA = parseFloat($(a).find(".diff-bleu").first().text());
-			var valueB = parseFloat($(b).find(".diff-bleu").first().text());
+		if($(this).children("span").text() == "DESC") {
+			callback = function sortDescending(a, b) {
+				var valueA = parseFloat($(a).find(".diff-bleu").first().text());
+				var valueB = parseFloat($(b).find(".diff-bleu").first().text());
 			
-			return valueA > valueB ? -1 : 1;
-		};
-		newOrder = "ASC";
-	} else {
-		callback = function (a, b) {
-			var valueA = parseFloat($(a).find(".diff-bleu").first().text());
-			var valueB = parseFloat($(b).find(".diff-bleu").first().text());
+				return valueA > valueB ? -1 : 1;
+			};
+			newOrder = "ASC";
+		} else {
+			callback = function (a, b) {
+				var valueA = parseFloat($(a).find(".diff-bleu").first().text());
+				var valueB = parseFloat($(b).find(".diff-bleu").first().text());
 			
-			return valueA < valueB ? -1 : 1;
-		};
-		newOrder = "DESC";
+				return valueA < valueB ? -1 : 1;
+			};
+			newOrder = "DESC";
+		}
+		
+		$("ul#sentences li").sort(callback);
+		$(this).children("span").text(newOrder);
+
+		return false;
+	});
+
+	$("#diff1-control").click(function() {
+		toggleDiff($(this), ".tst1 ins, .tst1 del");
+		
+		return false;
+	});
+
+	$("#diff2-control").click(function() {
+		toggleDiff($(this), ".tst2 ins, .tst2 del");
+		
+		return false;	
+	});
+	
+	function toggleDiff($control, selector) {
+		if( $control.children("span").text() == "Display" ) {
+			$(selector).addClass("active");
+			$control.children("span").text("Hide");
+		} else {
+			$(selector).removeClass("active");
+			$control.children("span").text("Display");
+		}
 	}
-		
-	$("ul#sentences li").sort(callback);
-	$(this).children("span").text(newOrder);
-
-	return false;
-});
-
-
-$("#diff1-control").click(function() {
-	$(".tst1 ins").css({
-		"display" : "inline",
-		"background-color" : "red",	
-	});
-	$(".tst1 del").css({
-		"display" : "inline",
-		"background-color" : "blue",
-	});
-});
-
-
-$("#diff1-control").click(function() {
-	$(".tst2 ins").css({
-		"display" : "inline",
-		"background-color" : "red",	
-	});
-	$(".tst2 del").css({
-		"display" : "inline",
-		"background-color" : "blue",
+	
+	
+	$(".display-diff").click(function() {
+		$(this).closest("tr").find("ins, del").toggleClass("active");
+	
+		return false;
 	});
 });
