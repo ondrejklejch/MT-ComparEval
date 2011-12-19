768fa1daf06b5259c835a8200d8dcd67c786cf28
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-12-19 09:21:50 +0100

    Sessions added to the application

--- lib/MTComparEval.pm
+++ lib/MTComparEval.pm
@@ -20,6 +20,9 @@ use Catalyst qw/
     -Debug
     ConfigLoader
     Static::Simple
+    Session
+    Session::State::Cookie
+    Session::Store::FastMmap
 /;
 
 extends 'Catalyst';
@@ -40,6 +43,7 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
+    session => { flash_to_stash => 1 },
 );
 
 # Start the application
