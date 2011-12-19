99e0824e8fc389e306212f1708bad10fff6ddd72
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-12-19 13:07:07 +0100

    Default url now shows experiments list.

--- lib/MTComparEval/Controller/Root.pm
+++ lib/MTComparEval/Controller/Root.pm
@@ -29,8 +29,7 @@ The root page (/)
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    # Hello World
-    $c->response->body( $c->welcome_message );
+    $c->response->redirect( $c->uri_for_action( 'experiments/index' ) );
 }
 
 =head2 default
