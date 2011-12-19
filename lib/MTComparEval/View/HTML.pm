c50670110966e6b806e43038c661021803183b73
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-12-19 10:07:42 +0100

    View added.

---
+++ lib/MTComparEval/View/HTML.pm
@@ -0,0 +1,33 @@
+package MTComparEval::View::HTML;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+MTComparEval::View::HTML - TT View for MTComparEval
+
+=head1 DESCRIPTION
+
+TT View for MTComparEval.
+
+=head1 SEE ALSO
+
+L<MTComparEval>
+
+=head1 AUTHOR
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
