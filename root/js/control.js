9ba90f54df10d019f57017961243accab3512e7a
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-12-30 16:34:43 +0100

    JS scripts updated.

--- root/js/control.js
+++ root/js/control.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-	$("#sort-control").click(function() {
+	$("#sort-control").click(function( event ) {
+		event.preventDefault();
+       		if( $(this).hasClass( "processing" ) ) {
+			return;
+		}
+
+		var className = "." + $(this).attr( 'class' );
 		$(this).addClass( "processing" );
-		
+				
 		var callback;
 		var newOrder;
 		
 		if($(this).children("span").text() == "DESC") {
 			callback = function sortDescending(a, b) {
-				var valueA = parseFloat($(a).find(".bleu").first().text());
-				var valueB = parseFloat($(b).find(".bleu").first().text());
+				var valueA = parseFloat($(a).find( className ).first().text());
+				var valueB = parseFloat($(b).find( className ).first().text());
 			
 				return valueA > valueB ? -1 : 1;
 			};
 			newOrder = "ASC";
 		} else {
 			callback = function (a, b) {
-				var valueA = parseFloat($(a).find(".bleu").first().text());
-				var valueB = parseFloat($(b).find(".bleu").first().text());
+				var valueA = parseFloat($(a).find( className ).first().text());
+				var valueB = parseFloat($(b).find( className ).first().text());
 			
 				return valueA < valueB ? -1 : 1;
 			};
@@ -30,20 +36,37 @@ $(document).ready(function() {
 		return false;
 	});
 
-	$("#diff1-control").click(function() {
+	$("#diff-control").click(function() {
 		$(this).addClass( "processing" );
-		generateDiff( $(this), ".tst" );
+		generateDiff( $(this), ".tst", ".ref" );
 		toggleDiff($(this), ".tst");
 		$(this).removeClass( "processing" );
 		
 		return false;
 	});
 	
-	
-	function generateDiff( $controler, selector ) {
-		if( ! $controler.hasClass( "cached" ) ) { 
-			$(selector).find("td.text").each( function( index, element ) {
-				computeDiff( element );		
+	$("#diff1-control").click(function() {
+		$(this).addClass( "processing" );
+		generateDiff( $(this), ".tst1", ".tst2" );
+		toggleDiff($(this), ".tst1");
+		$(this).removeClass( "processing" );
+		
+		return false;
+	});
+
+	$("#diff2-control").click(function() {
+		$(this).addClass( "processing" );
+		generateDiff( $(this), ".tst2", ".tst1" );
+		toggleDiff($(this), ".tst2");
+		$(this).removeClass( "processing" );
+		
+		return false;
+	});
+
+	function generateDiff( $controler, sourceSelector, referenceSelector ) {
+		if( !$controler.hasClass( "cached" ) ) { 
+			$(sourceSelector).find("td.text").each( function( index, element ) {
+				computeDiff( element, referenceSelector );		
 			});
 
 			$controler.addClass( "cached" );
@@ -62,20 +85,15 @@ $(document).ready(function() {
 	}
 	
 	
-	$(".display-diff").click(function() {
-		computeDiff( this );			
-		$(this).closest("tr").toggleClass( "active" );
-	
-		return false;
-	});
-	
-	
-	function computeDiff( element ) {
+	function computeDiff( element, referenceSelector ) {
 		var $tr = $(element).closest("tr");
 		
 		if( !$tr.hasClass( "cached" ) ) {
 			var $td = $tr.find("td.text");
-			var ref = $(element).closest("table").find("tr.ref td.text").text();
+			var $refNode = $(element).closest("table").find("tr" + referenceSelector + " td.text").clone();
+			$refNode.children( "del" ).remove();
+	
+			var ref = $refNode.text();
 			var tst = $td.text();
 		
 			$tr.addClass("cached");
