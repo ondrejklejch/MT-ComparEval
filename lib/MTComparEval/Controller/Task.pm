96f723ab5df91e4526e77a153f66bcaa72263490
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-12-30 13:50:31 +0100

    Task detail fixed

--- lib/MTComparEval/Controller/Task.pm
+++ lib/MTComparEval/Controller/Task.pm
@@ -29,30 +29,6 @@ sub index :Path :Args(0) {
 }
 
 
-sub loadSentences {
-    my( $filename ) = @_;
-    open my $file, '<', $filename or die "$filename not exits";
-
-    my @senteces = ();
-    while( <$file> ) {
-        chomp;
-        push @senteces, $_;
-    }
-
-    return @senteces;
-}
-
-
-sub get_bleu_for_sentence {
-	my ( $ref, $tst ) = @_;
-
-	my $ngram = new NGram;
-	$ngram->add_sentence( $ref, $tst );
-
-	return sprintf("%.4f", $ngram->get_sentence_bleu() );
-}
-
-
 =head2 detail
 
 =cut
@@ -61,31 +37,28 @@ sub detail :Local :Args(1) {
     my( $self, $c, $id ) = @_;
 
     my $task = $c->model( 'DBIC::tasks' )->find( { id => $id } );
-    my $experiment = $c->model( 'DBIC::experiments' )->find( { id => $task->experiment_id } );
-    my @sourceSentences = loadSentences( $c->path_to( 'data', 'source' . $experiment->id ) );
-    my @referenceSentences = loadSentences( $c->path_to( 'data', 'reference' . $experiment->id ) );
-    my @translationSentences = loadSentences( $c->path_to( 'data', 'translation' . $task->id ) );
+    my $experiment_id = $task->get_column( 'experiment_id' );
+    my $experiment = $c->model( 'DBIC::experiments' )->find( { id => $experiment_id } );
+    my $sourceSentences = $experiment->search_related( 'source_sentences' );
+    my $referenceSentences = $experiment->search_related( 'reference_sentences' );
+    my $translationSentences = $task->search_related( 'translation_sentences' );
     
-    my $ngrams = new NGram;
-    my @sentences = ();
-    while( $#sourceSentences >= 0 ) {
-        my $source = pop @sourceSentences;
-        my $reference = pop @referenceSentences;
-        my $translation = pop @translationSentences;
-
-        $ngrams->add_sentence( $reference, $translation );
+    my @sentences;
+    while( my $translation = $translationSentences->next() ) {
+        my $source = $sourceSentences->next();
+        my $reference = $referenceSentences->next();
 
         my %sentence = (
-            'src' => $source,
-            'ref' => $reference,
-            'tst' => $translation,
-            'bleu' => get_bleu_for_sentence( $reference, $translation ),
+            'src' => $source->get_column( 'text' ),
+            'ref' => $reference->get_column( 'text' ),
+            'tst' => $translation->get_column( 'text' ),
+            'bleu' => $translation->get_column( 'diff_bleu' ),
         );
 
         push @sentences, \%sentence;
     }
 
-    $c->stash->{ 'bleu' } = $ngrams->get_bleu();
+    $c->stash->{ 'bleu' } = $task->get_column( 'bleu' );
     $c->stash->{ 'sentences' } = \@sentences;
 }
 
