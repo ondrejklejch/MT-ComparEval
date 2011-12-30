d590c7e7383970a2a249b581f3a218a2d37e08c5
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-12-30 16:35:27 +0100

    Comparison for two translations added.

--- lib/MTComparEval/Controller/Task.pm
+++ lib/MTComparEval/Controller/Task.pm
@@ -63,6 +63,55 @@ sub detail :Local :Args(1) {
 }
 
 
+sub compare :Local {
+    my( $self, $c, $id ) = @_;
+
+    my @tasks = $c->req->param( 'task' );
+    $c->log->info( scalar @tasks );
+    if( scalar @tasks != 2 ) {
+        $c->flash->{ error } = "You can compare only 2 tasks.";
+        $c->response->redirect( $c->req->referer );
+        $c->detach();
+    }
+   
+    my $task1 = $c->model( 'DBIC::tasks' )->find( { id => $tasks[0] } );
+    my $task2 = $c->model( 'DBIC::tasks' )->find( { id => $tasks[1] } );
+    if( $task1->get_column( 'experiment_id' ) != $task2->get_column( 'experiment_id' ) ) {
+        $c->flash->{ error } = "You can compare tasks only within one experiment.";
+        $c->response->redirect( $c->uri_for( '../experiments/' ) );
+        $c->detach();
+    }
+    
+    my $experiment_id = $task1->get_column( 'experiment_id' );
+    my $experiment = $c->model( 'DBIC::experiments' )->find( { id => $experiment_id } );
+    my $sourceSentences = $experiment->search_related( 'source_sentences' );
+    my $referenceSentences = $experiment->search_related( 'reference_sentences' );
+    my $translation1Sentences = $task1->search_related( 'translation_sentences' );
+    my $translation2Sentences = $task2->search_related( 'translation_sentences' );
+
+    my @sentences;
+    while( my $translation1 = $translation1Sentences->next() ) {
+        my $translation2 = $translation2Sentences->next();
+        my $source = $sourceSentences->next();
+        my $reference = $referenceSentences->next();
+
+        my %sentence = (
+            'src' => $source->get_column( 'text' ),
+            'ref' => $reference->get_column( 'text' ),
+            'tst1' => $translation1->get_column( 'text' ),
+            'bleu1' => $translation1->get_column( 'diff_bleu' ),
+            'tst2' => $translation2->get_column( 'text' ),
+            'bleu2' => $translation2->get_column( 'diff_bleu' ),
+        );
+
+        push @sentences, \%sentence;
+    }
+
+    $c->stash->{ 'task1' } = $task1;
+    $c->stash->{ 'task2' } = $task2;
+    $c->stash->{ 'sentences' } = \@sentences;
+}
+
 =head1 AUTHOR
 
 A clever guy
