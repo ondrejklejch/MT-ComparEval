3de669966c626c01c5953886a0620f2d7e1ffc3a
Ondřej Klejch <ondrej.klejch@gmail.com>
2012-06-11 14:38:50 +0200

    Preserve case condition added

--- mteval/evaluate.sh
+++ mteval/evaluate.sh
@@ -5,7 +5,7 @@ files=`ls -1 data/ | grep -Po "[A-Z]*" | uniq`
 echo "translator\tMTEval 11b\tMT-ComparEval"
 for system in $files
 do
-	MTEval=`./mteval-v11b.pl -b -r data/sample_reference.sgm -s data/sample_source.sgm -t data/sample_$system.sgm | grep "BLEU score" | grep -Po "[0-9].[0-9]{4}"`
+	MTEval=`./mteval-v11b.pl -cb -r data/sample_reference.sgm -s data/sample_source.sgm -t data/sample_$system.sgm | grep "BLEU score" | grep -Po "[0-9].[0-9]{4}"`
 	MTComparEval=`perl -I ../lib/ bleu.pl data/sample_reference data/sample_$system`
 
 	echo "$system\t\t$MTEval\t\t$MTComparEval"
