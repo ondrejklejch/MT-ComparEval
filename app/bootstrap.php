4396e38acd1cb1d805c712c5847023bf67e21382
Ondřej Klejch <ondrej.klejch@gmail.com>
2012-06-26 19:31:05 +0200

    Routes added to bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@
  * My Application bootstrap file.
  */
 use Nette\Application\Routers\Route;
-
+use Nette\Application\Routers\CliRouter;
 
 // Load Nette Framework
 require LIBS_DIR . '/Nette/loader.php';
@@ -29,10 +29,14 @@ $configurator->addConfig(__DIR__ . '/config/config.neon');
 $container = $configurator->createContainer();
 
 // Setup router
-$container->router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
-$container->router[] = new Route('api/<presenter>/<action>', array( 'module' => 'Api' ) );
-$container->router[] = new Route('<presenter>/<action>[/<id>]', 'Experiments:list');
-
+if( $container->params[ 'consoleMode' ] ) {
+	$container->router[] = new CliRouter();
+} else {
+	$container->router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
+	$container->router[] = new Route('workers/<presenter>/<action>', array( 'module' => 'Workers' ) );
+	$container->router[] = new Route('api/<presenter>/<action>', array( 'module' => 'Api' ) );
+	$container->router[] = new Route('<presenter>/<action>[/<id>]', 'Experiments:list');
+}
 
 // Configure and run the application!
 $container->application->run();
