6a2418cbf25c218919b7b9dc071ef05093cf2e8e
Ondřej Klejch <ondrej.klejch@gmail.com>
2012-07-09 19:10:37 +0200

    Tasks compare sql fixed.

--- app/models/Sentences.php
+++ app/models/Sentences.php
@@ -35,9 +35,9 @@ class Sentences {
 		$sql = 'SELECT t1.text AS tst1, t1.diff_bleu AS bleu1, t2.text AS tst2, t2.diff_bleu AS bleu2, r.text AS ref, s.text AS src '
 			. 'FROM translation_sentences AS t1 '
 			. 'JOIN source_sentences AS s ON s.position = t1.position '
-			. 'JOIN reference_sentences AS r ON r.position = t1.position '
+			. 'JOIN reference_sentences AS r ON r.position = t1.position AND r.experiment_id = s.experiment_id '
 			. 'JOIN translation_sentences AS t2 ON t2.position = t1.position '
-			. 'JOIN tasks AS ta ON ta.id = t.task_id AND ta.experiment_id = r.experiment_id '
+			. 'JOIN tasks AS ta ON ta.id = t1.task_id AND ta.experiment_id = r.experiment_id '
 			. 'WHERE t1.task_id = ? AND t2.task_id = ?';
 
 		return $this->connection->query( $sql, $id1, $id2 );
