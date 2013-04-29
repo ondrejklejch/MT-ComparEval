79cecb87e6129287a48f27fc8343bd0a181ccea6
Ondrej Klejch <ondrej.klejch@gmail.com>
2013-04-29 14:58:28 +0200

    Deploy script fixed.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,11 +16,11 @@ sqlite3 app/database < schema.sql
 # REMOVE LOCKS FROM DATA SO IT CAN BE IMPORTED AGAIN
 find data -name ".*" -exec rm {} \;
 
-# SET WRITE PERMISSIONS
-chmod -R 777 log/ temp/
+# INVALIDATE CACHE
+rm -rf temp/*
 
 # RUN BACKGROUND WORKERS
-php -f www/index.php Background:Watcher:Watch --folder=data >log/import.log 2>&1 &
+php -f www/index.php Background:Watcher:Watch --folder=data >log/experiments.log 2>&1 &
 
 echo DEPLOY DONE
 
