ee9bfbfcf5e9d136ce16bff462d9adeb532ea535
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-06-29 19:10:44 +0200

    Title can be set in the configuration.

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,6 +8,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 
 	public function beforeRender() {
 		$parameters = $this->context->getParameters();
+		$this->template->title = $parameters[ "title" ];
 		$this->template->showAdministration = $parameters[ "show_administration" ];
 	}
 
