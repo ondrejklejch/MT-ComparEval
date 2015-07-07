5555ba05baf3b4d927a6345b23b7381e3e23c1ac
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-07-07 14:18:36 +0200

    It is possible to switch between chart types.

--- www/js/directives.js
+++ www/js/directives.js
@@ -7,26 +7,27 @@ angular.module( 'MT-ComparEval', [] )
 			replace: true,
 
 			link: function (scope, element, attrs) {
-				var chartsDefaults = {
-					chart: {
-						renderTo: element[0],
-						type: attrs.type || null,
-						height: attrs.height || null,
-						width: attrs.width || null,
-					}
-				};
-
 				//Update when charts data changes
-				scope.$watch(function() { return attrs.value; }, function(value) {
+				scope.$watch(function() { return attrs.value + attrs.type; }, function(value) {
 					if(!attrs.value) return;
-						// We need deep copy in order to NOT override original chart object.
-						// This allows us to override chart data member and still the keep
-						// our original renderTo will be the same
-						var deepCopy = true;
-						var newSettings = {};
-						$.extend(deepCopy, newSettings, chartsDefaults, JSON.parse(attrs.value));
 
-						var chart = new Highcharts.Chart(newSettings);
+					var chartsDefaults = {
+						chart: {
+							renderTo: element[0],
+							type: attrs.type || null,
+							height: attrs.height || null,
+							width: attrs.width || null,
+						}
+					};
+
+					// We need deep copy in order to NOT override original chart object.
+					// This allows us to override chart data member and still the keep
+					// our original renderTo will be the same
+					var deepCopy = true;
+					var newSettings = {};
+					$.extend(deepCopy, newSettings, chartsDefaults, JSON.parse(attrs.value));
+
+					var chart = new Highcharts.Chart(newSettings);
 				});
 			}
 		}
