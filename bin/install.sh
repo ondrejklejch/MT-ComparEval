fd63401f8605f61e337a6c84102f9fb32596c72b
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-07-28 00:08:44 +0200

    Install script fixed.

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -sS https://getcomposer.org/installer | php
-php composer.phar install --no-dev
+php composer.phar update --no-dev
 chmod -R 777 temp log storage
 sqlite3 storage/database < schema.sql
