d7aced3a72323fd0bd2e52f33d1da96c19568196
Ondrej Klejch <ondrej.klejch@gmail.com>
2015-07-27 17:43:00 +0200

    schema.sql actualized.

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE "experiments" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "name" text NOT NULL,
   "url_key" text NOT NULL UNIQUE,
-  "description" text NOT NULL
+  "description" text NOT NULL,
+  "visible" integer(0) NULL
 );
 
 
@@ -20,7 +21,7 @@ CREATE TABLE "tasks" (
   "experiments_id" integer NOT NULL,
   "name" text NOT NULL,
   "url_key" text NOT NULL,
-  "description" text NULL,
+  "description" text NULL, "visible" integer(0) NULL,
   FOREIGN KEY ("experiments_id") REFERENCES "experiments" ("id") ON DELETE CASCADE,
   UNIQUE("experiments_id","url_key")
 );
@@ -41,11 +42,14 @@ CREATE TABLE "metrics" (
   "name" text NOT NULL
 );
 
-INSERT INTO "metrics" ("name") VALUES ("bleu");
-INSERT INTO "metrics" ("name") VALUES ("bleu-cis");
-INSERT INTO "metrics" ("name") VALUES ("random");
-INSERT INTO "metrics" ("name") VALUES ("random-cis");
-
+INSERT INTO "metrics" ("id", "name") VALUES (1,	'BLEU');
+INSERT INTO "metrics" ("id", "name") VALUES (2,	'BLEU-cis');
+INSERT INTO "metrics" ("id", "name") VALUES (3,	'PRECISION');
+INSERT INTO "metrics" ("id", "name") VALUES (4,	'PRECISION-cis');
+INSERT INTO "metrics" ("id", "name") VALUES (5,	'RECALL');
+INSERT INTO "metrics" ("id", "name") VALUES (6,	'RECALL-cis');
+INSERT INTO "metrics" ("id", "name") VALUES (7,	'F-MEASURE');
+INSERT INTO "metrics" ("id", "name") VALUES (8,	'F-MEASURE-cis');
 
 CREATE TABLE "translations_metrics" (
   "translations_id" integer NOT NULL,
@@ -64,6 +68,7 @@ CREATE TABLE "tasks_metrics" (
   FOREIGN KEY ("metrics_id") REFERENCES "metrics" ("id") ON DELETE CASCADE
 );
 
+
 CREATE TABLE "tasks_metrics_samples" (
   "tasks_id" integer NOT NULL,
   "metrics_id" integer NOT NULL,
@@ -73,6 +78,7 @@ CREATE TABLE "tasks_metrics_samples" (
   FOREIGN KEY ("metrics_id") REFERENCES "metrics" ("id") ON DELETE CASCADE
 );
 
+
 CREATE TABLE "confirmed_ngrams" (
   "translations_id" integer NOT NULL,
   "text" text NOT NULL,
@@ -81,6 +87,7 @@ CREATE TABLE "confirmed_ngrams" (
   FOREIGN KEY ("translations_id") REFERENCES "translations" ("id") ON DELETE CASCADE
 );
 
+
 CREATE TABLE "unconfirmed_ngrams" (
   "translations_id" integer NOT NULL,
   "text" text NOT NULL,
@@ -88,4 +95,3 @@ CREATE TABLE "unconfirmed_ngrams" (
   "position" integer NOT NULL,
   FOREIGN KEY ("translations_id") REFERENCES "translations" ("id") ON DELETE CASCADE
 );
--- 
