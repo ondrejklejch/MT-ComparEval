0466e51658b4edf2bff9429621569537005a11b8
Sudarikov Roman <sudarikov@ufal.mff.cuni.cz>
2015-10-14 15:51:26 +0200

    Adding Nette\Caching\Cache to Hjerson
    Closes #50

--- libs/Metrics/Hjerson.php
+++ libs/Metrics/Hjerson.php
@@ -8,10 +8,12 @@ class Hjerson implements IMetric {
 	private $referenceText = array();
 	private $translationText = array();
 	private $type = null;
+	private $cache = null;
 	private $externalCmd = "python external/hjerson+.py";
 
-	public function __construct($type = "") {
+	public function __construct($type = "", Nette\Caching\Cache $cache) {
 		$this->type = $type;
+		$this->cache = $cache;
 	}
 
 	public function init() {
@@ -27,45 +29,46 @@ class Hjerson implements IMetric {
 	}
 
 	public function getScore() {
-		$hashId = md5(time());
-		$reference = "temp/ref." . $hashId;
-		$hypothesis  = "temp/hyp." . $hashId;
-		$refTxt = implode("\n", $this->referenceText);
-		$hypTxt = implode("\n", $this->translationText);
-		if ($refTxt != "" && $hypTxt != ""){
-			$textsHash = md5('Hyp:' . $hypTxt . "Ref:" . $refTxt);
-			$temporaryResultsFile = "temp/hjerson." . $textsHash;
-			if (file_exists($temporaryResultsFile)){
-				$output = file($temporaryResultsFile);
-				foreach ($output as $line){
-					$tempArray = explode("\t", $line);
-					$typeName = trim(str_replace(":","",$tempArray[0]));
-					if ($typeName == $this->type){
-						return $tempArray[1]; // if you want to use % representation, use [2] instead
-					}
-				}
-			}
+		if (count($this->referenceText) == 0 || count($this->translationText) == 0) {
+			return -1;
+		}
 
-			file_put_contents($reference, $refTxt);
-			file_put_contents($hypothesis, $hypTxt);
-			$cmd = sprintf("%s -R %s -H %s", $this->externalCmd, $reference, $hypothesis);
-			exec($cmd, $output, $return);
-			unlink($reference);
-			unlink($hypothesis);
-			if ($return != 0){
-				return -1;
-			} else {
-				file_put_contents($temporaryResultsFile, implode("\n", $output));
-				foreach ($output as $line){
-					$tempArray = explode("\t", $line);
-					$typeName = trim(str_replace(":","",$tempArray[0]));
-					if ($typeName == $this->type){
-						return $tempArray[1]; // if you want to use % representation, use [2] instead
-					}
-				}
+		list($return, $output) = $this->cache->call(array($this, 'runExternalCommandOnSentences'), $this->referenceText, $this->translationText);
+		if ($return != 0){
+			return -1;
+		}
+
+		return $this->processOutput($output);
+	}
+
+	public function runExternalCommandOnSentences($referenceText, $translationText) {
+		$reference = $this->saveSentencesToFile($referenceText, "temp/ref");
+		$hypothesis  = $this->saveSentencesToFile($translationText, "temp/hyp");
+
+		$cmd = sprintf("%s -R %s -H %s", $this->externalCmd, $reference, $hypothesis);
+		exec($cmd, $output, $return);
+
+		unlink($reference);
+		unlink($hypothesis);
+
+		return array($return, $output);
+	}
+
+	private function saveSentencesToFile($sentences, $path) {
+		$path .= md5(time());
+		file_put_contents($path, implode("\n", $sentences));
+
+		return $path;
+	}
+
+	private function processOutput($output){
+		foreach ($output as $line){
+			$tempArray = explode("\t", $line);
+			$typeName = trim(str_replace(":","",$tempArray[0]));
+			if ($typeName == $this->type){
+				return $tempArray[1]; // if you want to use % representation, use [2] instead
 			}
 		}
-		return -1;
 	}
 
 }
