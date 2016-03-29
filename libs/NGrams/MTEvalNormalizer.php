074c0ac34f107140aa879e37a119fea58166e470
Ondrej Klejch <ondrej.klejch@gmail.com>
2016-03-29 10:11:43 +0100

    International tokenization and default tokenization can be used.

--- libs/NGrams/MTEvalNormalizer.php
+++ libs/NGrams/MTEvalNormalizer.php
@@ -8,21 +8,20 @@
 class MTEvalNormalizer implements INormalizer {
 
 	public function normalize( $sentence ) {
-		$normalized = "$sentence";
+		$normalized = " $sentence ";
 
 		$normalized = preg_replace( '/&quot;/', '"', $normalized ); # quote to "
 		$normalized = preg_replace( '/&amp;/', '&', $normalized ); # ampersand to &
 		$normalized = preg_replace( '/&lt;/', '<', $normalized ); # less-than to <
 		$normalized = preg_replace( '/&gt;/', '>', $normalized ); # greater-than to >
-		$normalized = preg_replace( '/&apos;/', "'", $normalized ); # apostrophe to '
 
-		$normalized = preg_replace( '/(\P{N})(\p{P})/u', '$1 $2 ', $normalized ); # tokenize punctuation unless preceded by a digit
-		$normalized = preg_replace( '/(\p{P})(\P{N})/u', ' $1 $2', $normalized ); # tokenize punctuation unless followed by a digit
-
-		$normalized = preg_replace( '/(\p{S})/u', ' $1 ', $normalized ); # tokenize symbols
-		$normalized = preg_replace( '/\p{Z}+/u', ' ', $normalized ); # one space only between words
-		$normalized = preg_replace( '/^\p{Z}+/u',  '', $normalized );  # no leading space
-		$normalized = preg_replace( '/\p{Z}+$/u' , '', $normalized );  # no trailing space
+		$normalized = preg_replace( '/([\{-\~\[-\` -\&\(-\+\:-\@\/])/u', ' $1 ', $normalized );   # tokenize punctuation
+		$normalized = preg_replace( '/([^0-9])([\.,])/u', '$1 $2 ', $normalized ); # tokenize period and comma unless preceded by a digit
+		$normalized = preg_replace( '/([\.,])([^0-9])/u', ' $1 $2', $normalized ); # tokenize period and comma unless followed by a digit
+		$normalized = preg_replace( '/([0-9])(-)/u', '$1 $2 ', $normalized ); # tokenize dash when preceded by a digit
+		$normalized = preg_replace( '/\s+/u', ' ', $normalized ); # one space only between words
+		$normalized = preg_replace( '/^\s+/u', '', $normalized ); # no leading space
+		$normalized = preg_replace( '/\s+$/u', '', $normalized ); # no trailing space
 
 		return $normalized;
 	}
